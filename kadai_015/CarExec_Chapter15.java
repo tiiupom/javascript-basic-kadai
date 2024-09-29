da1ad21c5153facaf82d0ba0097c743b09e57382
tiiupom <chococo.lav1013.pom@gmail.com>
2024-09-29 18:39:16 +0900

    課題15　提出

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,11 @@
+package text.kadai_015;
+
+public class CarExec_Chapter15 {
+	public static void main(String[] args) {
+		
+		Car_Chapter15 gear = new Car_Chapter15(1, 10);
+
+		gear.gearChange(3);
+		gear.run();
+	}
+}
